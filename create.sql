687272767c61d2286797155f2f2901742ced4122
DARBAR VALI K <84654043+Darbarvali@users.noreply.github.com>
2021-06-19 18:08:14 +0530

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,25 +1,4 @@
-
-create table publisher(
-    id integer primary key,
-    name text,
-    country text
-);
-
-
-create table books(
-    id integer primary key,
-    title text,
-    publisher integer
-);
-
-
-create table subjects(
-    id integer primary key,
-    name text
-);
-
-
-create table books_subjects(
-    book integer,
-    subject integer
-);
+CREATE TABLE publisher(id integer PRIMARY KEY,name text,country text);
+CREATE TABLE books(id integer PRIMARY KEY,title text,publisher integer,FOREIGN KEY (publisher) REFERENCES publisher(id));
+CREATE TABLE subjects(id integer PRIMARY KEY,name text);
+CREATE TABLE books_subjects(book integer,subject integer,FOREIGN KEY (book) REFERENCES books(id),FOREIGN KEY (subject) REFERENCES subjects(id));  
